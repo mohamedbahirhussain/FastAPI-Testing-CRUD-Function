706fdabcee97a530c6333f0085e62404ba919f25
Bahir Hussain <mbahirh624@gmail.com>
2025-02-14 15:11:00 +0300

    Test Default Gate

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 app = FastAPI()
-@app.get("/")
-def welcome():
-    return{"Welcome to MBH"}
+@app.get("/welcome/{name}")
+async def welcome(name):
+    return{"Hello " + name + " Welcome to ISDB Saudi Arabia"}
+
+
+bank = FastAPI()
+@bank.get("/bank/default")
+async def bankapp():
+    return "ISDB Saudi Arabia"
+@bank.get("/bankaccount/{account_id}/{account_name}")
+async def bankapp(account_id : int, account_name):
+    return "Account ID: " + str(account_id) + ": Account Name: " + account_name
